03fb828a0383993a2cf328d70aba221febba528e
Matthew D. Ball <matthew.ball@ibm.com>
2019-03-20 10:16:51 +0000

    feat(encode): added code for encoding terminal input to base64

--- src/bin/b64_encode.js
+++ src/bin/b64_encode.js
@@ -1,1 +1,38 @@
-process.exit(-1);
+const argv = require('minimist')(
+    process.argv.slice(2),
+    {
+      boolean: [
+          'file'
+      ],
+      default: {
+        file: false
+      }
+    }
+  );
+const readline = require('readline');
+
+const getDataFromTerminal = async () => {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      crlfDelay: Infinity
+    });
+    let file = '';
+    rl.on('line', (line) => file += line);
+    rl.once('close', () => resolve(Buffer.from(file)));
+  });
+};
+
+const getDataFromFile = async () => {};
+
+let data;
+if (!argv.file) {
+  data = getDataFromTerminal();
+} else {
+  data = getDataFromFile();
+}
+
+data
+  .then((fileBuffer) => Promise.resolve(fileBuffer.toString('base64')))
+  .then((encodedData) => {console.log(encodedData); process.exit(0);});
