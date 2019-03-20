903f7952a9c1d8b3f89e5371306fb98d1ebd230b
Matthew D. Ball <matthew.ball@ibm.com>
2019-03-20 11:08:07 +0000

    refactor(terminal): shifted out the terminal read into its own file

--- src/bin/b64_encode.js
+++ src/bin/b64_encode.js
@@ -8,20 +8,7 @@ const argv = require('minimist')(
     }
   );
 const fs = require('fs').promises;
-const readline = require('readline');
-
-const getDataFromTerminal = async () => {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      crlfDelay: Infinity
-    });
-    let file = '';
-    rl.on('line', (line) => file += line);
-    rl.once('close', () => resolve(Buffer.from(file)));
-  });
-};
+const { getDataFromTerminal } = require('../lib/terminal');
 
 const getDataFromFile = async () => {
   return fs.readFile(argv.file);
